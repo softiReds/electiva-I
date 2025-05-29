79937ffd045ae4e09e4a1106ddbea1119d84b116
Santiago Rojas <santirojastr12@gmail.com>
2025-05-28 23:10:52 -0500

    Add database context and configure JSON options in API
    Integrated `ElectivaIContext` with SQL Server connection and added JSON serialization settings to handle null values, references, and naming policy. Also introduced endpoint `/bdConexion` to ensure database creation and verify connection. Updated project file with necessary Entity Framework Core packages.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using ElectivaI.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ElectivaIContext>(e =>
+    e.UseSqlServer("Server=3.135.183.231,1433;Database=ElectivaI;User Id=sa;Password=Electiva1; TrustServerCertificate=True;"));
 
 var app = builder.Build();
 
@@ -14,5 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/bdConexion", ([FromServices] ElectivaIContext context) =>
+{
+    context.Database.EnsureCreated();
+    return Task.FromResult(Results.Ok("Conexión exitosa"));
+});
+
+app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
